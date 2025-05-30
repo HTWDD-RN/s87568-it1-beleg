7aca5654ac87fcb057be7045203d56244dda06f2
joshua-cloud-dot <s87568@htw-dresden.de>
2025-05-30 14:57:37 +0200

    add service worker for PWA

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,20 @@ import m from "./model.js";
 import p from "./presenter.js";
 import View from "./view.js";
 
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/scripts/service-worker.js')
+    .then(reg => console.log('Service Worker registered', reg))
+    .catch(err => console.error('Service Worker registration failed:', err));
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
+    
+
+
     let v = new View(p);
 
     // model loads some data async, so wait for it to finish loading
